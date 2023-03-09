867e8cbc52b6369fa484924befb8b8b533b05524
Pedro Lessa <pepeulessa@hotmail.com>
2023-03-09 18:01:04 -0300

    Funcs. de Cálculo Concluídas

--- main.js
+++ main.js
@@ -2,55 +2,99 @@ listaOperadores = document.querySelectorAll('.botao-func')
 listaNumeros = document.querySelectorAll('.botao-numero')
 tela = document.querySelector('.resultado')
 result = document.querySelector('.botao-result')
+reset = document.querySelector('.botao-reset')
+del = document.querySelector('.botao-del')
+memoria = []
 
+// FUNÇÃO QUE IDENTIFICA O NÚMERO E IMPRIME NA TELA
 listaNumeros.forEach(elemento => {
     elemento.addEventListener('click', evento =>{
+        
         if(tela.innerHTML === '0'){
             tela.innerHTML = elemento.value
         }
         else{
             tela.innerHTML += elemento.value
         }
-
     })
 });
 
+// FUNÇÃO QUE IDENTIFICA E ARMAZENA OS VALORES E OPERADORES, ALÉM DE PROCESSAR AS CONTAS
 listaOperadores.forEach(elemento => {
     elemento.addEventListener('click', ()=>{
-        if(elemento.value === '+'){
-            operador = '+'
-        }
-        else if(elemento.value === '-'){
-            operador = '-'
-        }
-        else if(elemento.value === '/'){
-            operador = '/'
-        }
-        else if(elemento.value === 'x'){
-            operador = 'x'
+
+        var operador = elemento.value
+        if(tela.innerHTML != 0){
+            memoria.push(parseFloat(tela.innerHTML))
+
+            if(operador === '+'){
+                memoria.push(operador)
+            }
+            else if(operador === '-'){
+                memoria.push(operador)
+            }
+            else if(operador === '/'){
+                memoria.push(operador)
+            }
+            else if(operador === 'x'){
+                memoria.push(operador)
+            }
         }
         
-        n1 = parseFloat(tela.innerHTML)
         tela.innerHTML = 0
+
+        if(memoria.length > 2){
+            var operadorMemoria = memoria[1]
+            if(operadorMemoria === '+'){
+                memoria[0] += memoria[2]
+            }
+            else if(operadorMemoria === '-'){
+                memoria[0] -= memoria[2]
+            }
+            else if(operadorMemoria === 'x'){
+                memoria[0] *= memoria[2]
+            }
+            else if(operadorMemoria === '/'){
+                memoria[0] /= memoria[2]
+            }
+
+            memoria.splice(1,2)
+        } 
     })
 });
 
-result.addEventListener('click', ()=>{
-    n2 = parseFloat(tela.innerHTML)
-    geraResultado(n1, operador, n2)
+// FUNCIONALIDADE DO BOTÃO DE DELETE
+del.addEventListener('click', ()=>{
+    var tamanho = tela.innerHTML.length
+    if(tamanho > 1){
+        tela.innerHTML = tela.innerHTML.substr(0,tamanho-1)
+    }
+    else{
+        tela.innerHTML = 0
+    }
 })
 
-function geraResultado(n1, operador, n2){
-    if(operador === '+'){
-        tela.innerHTML = n1 + n2
+// FUNCIONALIDADE DO BOTÃO DE RESET
+reset.addEventListener('click', ()=>{
+    tela.innerHTML = 0
+    memoria = []
+    console.log(tela.innerHTML, memoria)
+})
+
+// FUNCIONALIDADE DO BOTÃO DE RESULTADO
+result.addEventListener('click', ()=>{
+    if(memoria[1] === '+'){
+        tela.innerHTML = memoria[0] + parseFloat(tela.innerHTML)
     }
-    else if(operador === '-'){
-        tela.innerHTML = n1- n2    
+    else if(memoria[1] === '-'){
+        tela.innerHTML = memoria[0] - parseFloat(tela.innerHTML)
     }
-    else if(operador === 'x'){
-        tela.innerHTML = n1 * n2
+    else if(memoria[1] === 'x'){
+        tela.innerHTML = memoria[0] * parseFloat(tela.innerHTML)
     }
-    else if(operador === '/'){
-        tela.innerHTML = n1 / n2
+    else if(memoria[1] === '/'){
+        tela.innerHTML = memoria[0] / parseFloat(tela.innerHTML)
     }
-}
+    
+    memoria = []
+})
