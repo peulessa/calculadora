6d142c1645b1bc7e2c31c4652567822942b9e963
Pedro Lessa <pepeulessa@hotmail.com>
2023-03-10 18:27:54 -0300

    Toggle Finalizado e Funcionando

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-listaOperadores = document.querySelectorAll('.botao-func')
-listaNumeros = document.querySelectorAll('.botao-numero')
-tela = document.querySelector('.resultado')
-result = document.querySelector('.botao-result')
-reset = document.querySelector('.botao-reset')
-del = document.querySelector('.botao-del')
-memoria = []
+var listaOperadores = document.querySelectorAll('.botao-func')
+var listaNumeros = document.querySelectorAll('.botao-numero')
+var tela = document.querySelector('.resultado')
+var result = document.querySelector('.botao-result')
+var reset = document.querySelector('.botao-reset')
+var del = document.querySelector('.botao-del')
+var memoria = []
+var buttons = document.getElementsByClassName("button");
+var arr = [...buttons];
 
 // FUNÇÃO QUE IDENTIFICA O NÚMERO E IMPRIME NA TELA
 listaNumeros.forEach(elemento => {
@@ -43,7 +45,7 @@ listaOperadores.forEach(elemento => {
         
         tela.innerHTML = 0
 
-        if(memoria.length > 2){
+        if(memoria.length > 3){
             var operadorMemoria = memoria[1]
             if(operadorMemoria === '+'){
                 memoria[0] += memoria[2]
@@ -98,3 +100,131 @@ result.addEventListener('click', ()=>{
     
     memoria = []
 })
+
+// TOGGLE 
+arr.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        element.style.opacity = "1";
+
+        var principal = document.querySelector('.container-principal')
+        var cabecalho = document.querySelector('.container-cabecalho')
+        var telaStyle = document.querySelector('.container-tela')
+        var teclas = document.querySelector('.container-teclas')
+        var botaoDel = document.querySelector('.botao-del')
+        var botaoReset = document.querySelector('.botao-reset') 
+        var botaoResult = document.querySelector('.botao-result')
+        var body = document.querySelector('body')
+        var toggleBg = document.querySelector('.tri-state-toggle')
+
+        var toggleBtn = document.querySelectorAll('.button')
+
+        if(index == 0){
+            principal.classList.remove('container-principal2')
+            principal.classList.remove('container-principal3')
+
+            cabecalho.classList.remove('container-cabecalho2')
+            cabecalho.classList.remove('container-cabecalho3')
+
+            telaStyle.classList.remove('container-tela2')
+            telaStyle.classList.remove('container-tela3')
+
+            teclas.classList.remove('container-teclas2')
+            teclas.classList.remove('container-teclas3')
+
+            botaoDel.classList.remove('botao-del2')
+            botaoDel.classList.remove('botao-del3')
+
+            botaoReset.classList.remove('botao-reset2')
+            botaoReset.classList.remove('botao-reset3')
+
+            botaoResult.classList.remove('botao-result2')
+            botaoResult.classList.remove('botao-result3')
+
+            toggleBg.classList.remove('tri-state-toggle2')
+            toggleBg.classList.remove('tri-state-toggle3')
+
+            body.classList.remove('body2')
+            body.classList.remove('body3')
+
+            toggleBtn.forEach(element => {
+                element.classList.remove('button2')
+                element.classList.remove('button3')
+            })
+        }
+        else if(index == 1){
+            principal.classList.add('container-principal2')
+            principal.classList.remove('container-principal3')
+
+            cabecalho.classList.add('container-cabecalho2')
+            cabecalho.classList.remove('container-cabecalho3')
+
+            telaStyle.classList.add('container-tela2')
+            telaStyle.classList.remove('container-tela3')
+
+            teclas.classList.add('container-teclas2')
+            teclas.classList.remove('container-teclas3')
+
+            botaoDel.classList.add('botao-del2')
+            botaoDel.classList.remove('botao-del3')
+
+            botaoReset.classList.add('botao-reset2')
+            botaoReset.classList.remove('botao-reset3')
+
+            botaoResult.classList.add('botao-result2')
+            botaoResult.classList.remove('botao-result3')
+
+            toggleBg.classList.add('tri-state-toggle2')
+            toggleBg.classList.remove('tri-state-toggle3')
+
+            body.classList.add('body2')
+            body.classList.remove('body3')
+
+            toggleBtn.forEach(element => {
+                element.classList.add('button2')
+                element.classList.remove('button3')
+            })
+
+        }
+        else{
+            principal.classList.add('container-principal3')
+            principal.classList.remove('container-principal2')
+
+            cabecalho.classList.add('container-cabecalho3')
+            cabecalho.classList.remove('container-cabecalho2')
+
+            telaStyle.classList.add('container-tela3')
+            telaStyle.classList.remove('container-tela2')
+
+            teclas.classList.add('container-teclas3')
+            teclas.classList.remove('container-teclas2')
+
+            botaoDel.classList.add('botao-del3')
+            botaoDel.classList.remove('botao-del2')
+
+            botaoReset.classList.add('botao-reset3')
+            botaoReset.classList.remove('botao-reset2')
+
+            botaoResult.classList.add('botao-result3')
+            botaoResult.classList.remove('botao-result2')
+
+            toggleBg.classList.add('tri-state-toggle3')
+            toggleBg.classList.remove('tri-state-toggle2')
+
+            body.classList.add('body3')
+            body.classList.remove('body2')
+
+            toggleBtn.forEach(element => {
+                element.classList.add('button3')
+                element.classList.remove('button2')
+            })
+        }
+
+        arr
+            .filter(function (item) {
+            return item != element;
+            })
+            .forEach((item) => {
+            item.style.opacity = "0";
+            });
+        });
+});
